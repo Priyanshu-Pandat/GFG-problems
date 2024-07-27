a9d9d6d586541896deb5c01e06b862b2e5dda64a
Priyanshu-Pandat <109025781+Priyanshu-Pandat@users.noreply.github.com>
2024-07-27 21:00:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            Solution obj = new Solution();
+            double ans = obj.findSmallestMaxDist(a, k);
+            System.out.printf("%.2f", ans);
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+     public static class Pair {
+        double first;
+        int second;
+
+        Pair(double first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    public static double findSmallestMaxDist(int arr[], int k) {
+       int n = arr.length; // size of array.
+        int[] howMany = new int[n - 1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Double.compare(b.first, a.first));
+
+        // insert the first n-1 elements into pq
+        // with respective distance values:
+        for (int i = 0; i < n - 1; i++) {
+            pq.add(new Pair(arr[i + 1] - arr[i], i));
+        }
+
+        // Pick and place k gas stations:
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            // Find the maximum section
+            // and insert the gas station:
+            Pair tp = pq.poll();
+            int secInd = tp.second;
+
+            // insert the current gas station:
+            howMany[secInd]++;
+
+            double inidiff = arr[secInd + 1] - arr[secInd];
+            double newSecLen = inidiff / (double) (howMany[secInd] + 1);
+            pq.add(new Pair(newSecLen, secInd));
+        }
+
+        return pq.peek().first;
+        // code here
+    }
+}
